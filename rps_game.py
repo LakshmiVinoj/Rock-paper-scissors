59af83b7271f1e1c2361befa795540ba45fc5209
Lakshmi Vinoj <59805636+LakshmiVinoj@users.noreply.github.com>
2020-08-17 10:40:31 -0400

    Add files via upload

---
+++ rps_game.py
@@ -0,0 +1,72 @@
+from random import randint
+
+# global variables
+choices = ["r", "p", "s"]
+comp_play = choices[randint(0,2)]
+#***********************************************************************************************************************
+
+# user inputs
+player_name = input("enter player name: ")
+#***********************************************************************************************************************
+
+# keys
+print("r -> rock\np -> paper\ns -> scissors")
+#***********************************************************************************************************************
+
+# rock paper scissor working 
+def game():
+    rounds = int(input("enter no of rounds: "))
+    comp_score = 0
+    player_score = 0   
+    for i in range(0, rounds):
+        user_choice = input("your choice: ") 
+        if user_choice == comp_play:
+            print("tie")
+    
+        elif user_choice == "r":
+            if comp_play == "s":
+                player_score+=1
+                print("u won")
+            else:
+                comp_score+=1
+                print("u lost") 
+                
+        elif user_choice == "p":
+            if comp_play == "r":
+                player_score+=1
+                print("u won")
+            else:
+                comp_score+=1
+                print("u lost")        
+        else:
+            if comp_play == "r":
+                player_score+=1
+                print("u won")
+            else:
+                comp_score+=1
+                print("u lost")  
+                
+        #def score_display():
+        print("+"+ "-"*(len(player_name) + 13) + "+")
+        print("| computer | "+ player_name + " |")
+        print("|"+("-" * (len(player_name) + 13))+ "|")
+        print("| " + str(comp_score) +(" " * 8)+"| "+ str(player_score) +(" " *(len(player_name)-1))+" |")
+        print("+"+ "-"*(len(player_name) + 13) + "+")   
+    
+    def winner():
+        if player_score > comp_score:
+            print("The winner is " + player_name)
+        elif player_score < comp_score:
+            print("The winner is computer") 
+        else:
+            print("It is a tie")
+            
+    winner()
+        
+    n = int(input("rematch?\nyes -> 1\nno -> 0\nenter: "))
+    if n==1:
+        game()
+    
+#***********************************************************************************************************************
+
+game()
